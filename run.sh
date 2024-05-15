2dd12343cb3d5a4139e616cdf123692030c36bdf
jakobottar <jakobottar@pm.me>
2024-05-15 10:05:49 -0600

    remove ntfy script calls

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ conda activate mamba
 
 export CUDA_VISIBLE_DEVICES=0
 
-# ntfy "resnet18" python main.py --epochs 5 --arch resnet18
-# ntfy "resnet50" python main.py --epochs 25 --arch resnet50 --name rn50
-# ntfy "convnext" python main.py --epochs 25 --arch convnext --name convnext
-# ntfy "vit" python main.py --epochs 25 --arch vit --batch-size 128
-ntfy "vim" python main.py --epochs 1 --arch vim # --name vim
+# python main.py --epochs 5 --arch resnet18
+# python main.py --epochs 25 --arch resnet50 --name rn50
+# python main.py --epochs 25 --arch convnext --name convnext
+# python main.py --epochs 25 --arch vit --batch-size 128
+python main.py --epochs 1 --arch vim # --name vim
